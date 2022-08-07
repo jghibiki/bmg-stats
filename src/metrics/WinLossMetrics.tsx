3b4a73faf4954612b621c63bcfdc7884fc3bb039
jghibiki <jghibiki.games@gmail.com>
2022-08-06 19:49:49 -0500

    update wdl metrics

--- src/metrics/WinLossMetrics.tsx
+++ src/metrics/WinLossMetrics.tsx
@@ -27,30 +27,39 @@ const VictoryPointsReport: Component<{ event: EventMetadata, metrics: MetricsObj
     const [adjustedCrewOptions, setAdjustedCrewOptions] = createSignal()
     const [adjustedCrewSeries, setAdjustedCrewSeries] = createSignal()
 
+    const [wdlVpRatioOptions, setWdlVpRatioOptions] = createSignal()
+    const [wdlVpRatioSeries, setWdlVpRatioSeries] = createSignal()
+
     const [tpVpRatioOptions, setTpVpRatioOptions] = createSignal()
     const [tpVpRatioSeries, setTpVpRatioSeries] = createSignal()
 
 
     createEffect(() => {
+        const wdlIndexer = ["W", "D", "L"]
         var crewAggregations = new Map<string, Array<string>>()
         var crewPlayerCounts = new Map<string, Array<string>>()
-        var adjustedCrewAggregations = new Map<string, number>()
+        var adjustedCrewAggregations = new Array<number>()
         var vpAggs = new Map<string, number>()
+        var wdlVpAggs = new Map<string, Array<number>>()
         var tpAggs = new Map<string, number>()
         var tpToVpAggregations = new Map<string, number>()
 
         for (var metric of statsList()) {
             let player = metric.player
             let crew = metric.crew
+            let wdl = convertTp(+metric.tp)
+            let wdlIndex = wdlIndexer.indexOf(wdl)
 
-            if (crewAggregations.has(crew)) {
-                crewAggregations.get(crew)?.push(convertTp(+metric.tp))
-            }
-            else {
-                crewAggregations.set(
-                    crew, [convertTp(+metric.tp)]
-                )
-            }
+            crewAggregations.set(
+                crew,
+                [wdl, ...(crewAggregations.get(crew) || [])]
+            )
+
+            let _wdlVpAggs: Array<number> = wdlVpAggs.get(crew) || [0, 0, 0]
+            _wdlVpAggs[wdlIndex] += +metric.vp
+            wdlVpAggs.set(
+                crew, _wdlVpAggs
+            )
 
             vpAggs.set(
                 crew,
@@ -77,34 +86,43 @@ const VictoryPointsReport: Component<{ event: EventMetadata, metrics: MetricsObj
         var draws = Array<number>()
         var losses = Array<number>()
 
-        var adjustedWins = Array<number>()
-        var adjustedDraws = Array<number>()
-        var adjustedLosses = Array<number>()
+        var averageWins = Array<number>()
+        var averageDraws = Array<number>()
+        var averageLosses = Array<number>()
+
+        var vpWinRatio = Array<number>()
+        var vpDrawRatio = Array<number>()
+        var vpLossRatio = Array<number>()
 
         for (let el of crewAggregations) {
             const crew = el[0]
             const results = el[1]
             const playerCOunt = crewPlayerCounts.get(crew)!.length
+            const vpAgg = wdlVpAggs.get(crew)!
 
             const w = results.filter((e) => e === "W").length
             wins.push(w)
-            adjustedWins.push(+(w / playerCOunt).toFixed(2))
+            averageWins.push(+(w / playerCOunt).toFixed(2))
+            vpWinRatio.push(+(vpAgg[0] / w).toFixed(2))
+
             const d = results.filter((e) => e === "D").length
             draws.push(d)
-            adjustedDraws.push(+(d / playerCOunt).toFixed(2))
+            averageDraws.push(+(d / playerCOunt).toFixed(2))
+            vpDrawRatio.push(+(vpAgg[1] / d).toFixed(2))
+
             const l = results.filter((e) => e === "L").length
             losses.push(l)
-            adjustedLosses.push(+(l / playerCOunt).toFixed(2))
+            averageLosses.push(+(l / playerCOunt).toFixed(2))
+            vpLossRatio.push(+(vpAgg[2] / l).toFixed(2))
         }
 
         for (let el of tpAggs) {
             const crew = el[0]
             const tp = el[1]
             const vp = vpAggs.get(crew)!
-            const numPlayers = crewPlayerCounts.get(crew)!.length
 
             tpToVpAggregations.set(
-                crew, +((tp / vp)).toFixed(2)
+                crew, +((vp / tp)).toFixed(2)
             )
         }
 
@@ -158,17 +176,47 @@ const VictoryPointsReport: Component<{ event: EventMetadata, metrics: MetricsObj
         setAdjustedCrewSeries([
             {
                 name: "Wins",
-                data: adjustedWins
+                data: averageWins
             },
             {
                 name: "Draws",
-                data: adjustedDraws
+                data: averageDraws
             },
             {
                 name: "Losses",
-                data: adjustedLosses
+                data: averageLosses
             },
 
+        ])
+
+        setWdlVpRatioOptions({
+            chart: {
+                id: "chart",
+                width: "100%",
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true
+                }
+            },
+            xaxis: {
+                categories: Array.from(crewAggregations.keys())
+            },
+        })
+
+        setWdlVpRatioSeries([
+            {
+                "name": "Wins",
+                data: vpWinRatio
+            },
+            {
+                "name": "Draw",
+                data: vpDrawRatio
+            },
+            {
+                "name": "Loss",
+                data: vpLossRatio
+            },
         ])
 
         setTpVpRatioOptions({
@@ -182,7 +230,7 @@ const VictoryPointsReport: Component<{ event: EventMetadata, metrics: MetricsObj
         })
 
         setTpVpRatioSeries([{
-            name: 'One VP is worth X TP',
+            name: 'One TP is worth X VP',
             data: Array.from(tpToVpAggregations.values())
         }])
     })
@@ -196,8 +244,12 @@ const VictoryPointsReport: Component<{ event: EventMetadata, metrics: MetricsObj
             <h3>Average Crew W/D/L Stats</h3>
             <SolidApexCharts width="500" type="bar" options={adjustedCrewOptions()} series={adjustedCrewSeries()} />
 
-            <h3>TPs per VP ratio</h3>
-            <p>For one VP a player earns roughly X TP's</p>
+            <h3>Average VP by W/D/L</h3>
+            <p>Average number of VP earned by a crew when they won, drew, or lost.</p>
+            <SolidApexCharts width="500" type="bar" options={wdlVpRatioOptions()} series={wdlVpRatioSeries()} />
+
+            <h3>VPs per TP ratio</h3>
+            <p>One TP is worth roughly X VP's</p>
             <SolidApexCharts width="500" type="bar" options={tpVpRatioOptions()} series={tpVpRatioSeries()} />
         </>
     );
