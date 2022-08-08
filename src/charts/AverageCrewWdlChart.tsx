b3eaae93219abc663b48250ca09c70594109c39b
jghibiki <jghibiki.games@gmail.com>
2022-08-08 09:30:10 -0500

    fix average calculation for wdl

--- src/charts/AverageCrewWdlChart.tsx
+++ src/charts/AverageCrewWdlChart.tsx
@@ -11,8 +11,6 @@ const AverageCrewWdlChart: Component<{ metrics: Array<MatchResult> }> = (props) 
 
     createEffect(() => {
         var crewAggregations = new Map<string, Array<string>>()
-        const crewPlayerCounts = calculateCrewPlayerCounts(statsList())
-        const roundTracker = new Set()
 
         for (var metric of statsList()) {
             let crew = metric.crew
@@ -22,8 +20,6 @@ const AverageCrewWdlChart: Component<{ metrics: Array<MatchResult> }> = (props) 
                 crew,
                 [wdl, ...(crewAggregations.get(crew) || [])]
             )
-
-            roundTracker.add(metric.round)
         }
 
         var wins = Array<number>()
@@ -33,18 +29,16 @@ const AverageCrewWdlChart: Component<{ metrics: Array<MatchResult> }> = (props) 
         for (let el of crewAggregations) {
             const crew = el[0]
             const results = el[1]
-            const playerCount = crewPlayerCounts.get(crew)?.size || 0.0001
-            const rounds = roundTracker.size
 
 
             const w = results.filter((e) => e === "W").length
-            wins.push(+(w / playerCount / rounds * 100).toFixed(1))
+            wins.push(+(w / results.length * 100).toFixed(1))
 
             const d = results.filter((e) => e === "D").length
-            draws.push(+(d / playerCount / rounds * 100).toFixed(1))
+            draws.push(+(d / results.length * 100).toFixed(1))
 
             const l = results.filter((e) => e === "L").length
-            losses.push(+(l / playerCount / rounds * 100).toFixed(2))
+            losses.push(+(l / results.length * 100).toFixed(2))
         }
 
         setChartOptions({
