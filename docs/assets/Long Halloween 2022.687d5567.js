2061abacb0c41279f78e5f705f25541ba7b4e655
jghibiki <jghibiki.games@gmail.com>
2022-11-16 23:46:21 -0600

    update site

---
+++ docs/assets/Long Halloween 2022.687d5567.js
@@ -0,0 +1,1 @@
+const s=[{boss:"black canary",crew:"Birds of Prey"},{boss:"black mask",crew:"organized crime"},{boss:"poison ivy",crew:"birds of prey"}],o={bosses:s};export{s as bosses,o as default};
